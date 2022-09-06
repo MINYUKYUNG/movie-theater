fde5f0154092b591694d92fde422699dad9f22e0
wooyoung <dndud2906@naver.com>
2022-09-07 04:26:16 +0900

    🐛Fix: [#1] Router 에러 수정
    Router 에러 발생
    이유 : BrowserRouter 태그가 App.tsx에서 한번
    routes/index.tsx에서 한번 총 두번을 실행 하면서 에러 발생.
    
    해결 : App.tsx 에 있는 BrowserRouter를 지우고 routes/index.tsx로 옮김.
    
    git pull 후 충돌
    이유 : 다현님이랑 라우트 부분 및 사이드바 코드가 겹침.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,48 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from '@pages/Main';
+import {
+  HomePage,
+  NowPlayingPage,
+  UpComingPage,
+  TopRatedPage,
+  SearchMoviePage,
+  DetailPage,
+} from '@pages/index';
+import Nav from '@components/Nav';
+import styled from 'styled-components';
 import Layout from '@components/commons/layout';
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
+      <BodyContainer>
+        <Nav />
+        <Contents>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/now_play" element={<NowPlayingPage />} />
+            <Route path="/up_coming" element={<UpComingPage />} />
+            <Route path="/top_rated" element={<TopRatedPage />} />
+            <Route path="/search_movie" element={<SearchMoviePage />} />
+            <Route path="/detail/:id" element={<DetailPage />} />
+          </Routes>
+        </Contents>
+      </BodyContainer>
+      {/* <Route element={<Layout />}>
           <Route path="/" element={<Main />} />
           <Route path="*" element={<div>404</div>} />
-        </Route>
-      </Routes>
+        </Route> */}
     </BrowserRouter>
   );
 }
+
+const BodyContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+`;
+
+const Contents = styled.div`
+  background: #181818;
+  width: 100%;
+  overflow: scroll;
+`;
