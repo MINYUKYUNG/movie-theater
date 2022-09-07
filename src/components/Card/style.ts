66495d9e43db53be3ebb25f2ab39aa910dfe462b
wooyoung <dndud2906@naver.com>
2022-09-07 10:58:51 +0900

    ⚡️Modify: [#2] theme.ts파일 수정, 기존파일 수정
     1. theme.ts
      - font-size: px에서 rem으로 변경 -> 이유: 노트북에서 볼때 글씨가 너무 커서 맞췃다.
      - colors, fontSize 등 적용되어 있지 않던 파일들에 적용
    
     2. 기존파일 수정
      - 디테일 페이지에서 상영시간 및 개봉날짜 형식 변경 로직 추가 -> 이유: 투박하게 133, 2022-09-07 나와서 2시간13분, 2022년 9월 7일 처럼 수정
      - 영화 카드 마우스 호버 이벤트 추가 -> 이유: 마우스 호버 시 눌리는 느낌을 주고 싶어서 간결하게 추가해봤다.

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,20 +1,26 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  height: 25vw;
+  height: 21vw;
   margin-right: 30px;
+  margin-bottom: 40px;
+  &:hover {
+    position: relative;
+    top: 2px;
+  }
 `;
 
 export const Poster = styled.img`
   width: 100%;
+  height: 80%;
   border-radius: 10px;
   cursor: pointer;
 `;
 
 export const Title = styled.div`
-  font-size: 14px;
+  font-size: ${({ theme }) => theme.fontSize.SMALL};
   font-weight: 400;
-  color: #ffffff;
+  color: ${({ theme }) => theme.colors.WHITE};
   margin-top: 9px;
 `;
 
@@ -23,7 +29,9 @@ export const RateContainer = styled.div`
   flex-direction: row;
 `;
 export const Rate = styled.div`
-  font-size: 12px;
-  color: #ffb800;
+  font-size: ${({ theme }) => theme.fontSize.X_SMALL};
+  color: ${({ theme }) => theme.colors.MAIN};
   font-weight: 400;
+  margin-top: 3px;
+  margin-left: 2px;
 `;
--- src/pages/DetailPage/components/ExplainContainer/index.tsx
+++ src/pages/DetailPage/components/ExplainContainer/index.tsx
@@ -1,4 +1,6 @@
 import * as S from './style';
+import StarIcon from '../../../../assets/svgs/star-icon.svg';
+import { useCallback } from 'react';
 
 interface DetailMovie {
   title: string;
@@ -21,6 +23,19 @@ export default function ExplainContainer({
   languages,
   homepage,
 }: DetailMovie) {
+  const transTime = useCallback((time: number) => {
+    const hour = Math.floor(time / 60);
+    const minute = time % 60;
+
+    if (hour === 0) return minute + '분';
+    return hour + '시간 ' + minute + '분';
+  }, []);
+
+  const transDate = useCallback((date: string) => {
+    const dates = date.split('-');
+    return dates[0] + '년 ' + dates[1] + '월 ' + dates[2] + '일';
+  }, []);
+
   return (
     <S.ExplainContainer>
       <S.Title>{title}</S.Title>
@@ -28,30 +43,30 @@ export default function ExplainContainer({
         {genres.map((genre) => (
           <S.SubTitle key={genre.id}>{genre.name}</S.SubTitle>
         ))}
-        <S.SubTitle>{release_date}</S.SubTitle>
+        <S.SubTitle>{release_date.slice(0, 4)}</S.SubTitle>
       </S.SubTitleContainer>
       <S.OverView>{overview}</S.OverView>
       <S.FurtherContainer>
         <S.FurtherTitle>Runtime</S.FurtherTitle>
-        <S.FurtherContent>{runtime}</S.FurtherContent>
+        <S.FurtherContent>{transTime(runtime)}</S.FurtherContent>
       </S.FurtherContainer>
       <S.FurtherContainer>
         <S.FurtherTitle>Released</S.FurtherTitle>
-        <S.FurtherContent>{release_date}</S.FurtherContent>
+        <S.FurtherContent>{transDate(release_date)}</S.FurtherContent>
       </S.FurtherContainer>
       <S.FurtherContainer>
         <S.FurtherTitle>Rating</S.FurtherTitle>
-        <S.FurtherContent>{rate}</S.FurtherContent>
+        <S.FurtherContent>
+          <img src={StarIcon} alt="star_icon" />
+          {rate}
+        </S.FurtherContent>
       </S.FurtherContainer>
       <S.FurtherContainer>
         <S.FurtherTitle>Language</S.FurtherTitle>
-        {/* {languages.map((language, i) => (
-          <S.FurtherContent key={i}>{language.english_name}</S.FurtherContent>
-        ))} */}
         <S.FurtherContent>{languages[0].english_name}</S.FurtherContent>
       </S.FurtherContainer>
       <S.FurtherContainer>
-        <S.FurtherTitle>Hompage</S.FurtherTitle>
+        <S.FurtherTitle>Homepage</S.FurtherTitle>
         <S.FurtherContent>{homepage}</S.FurtherContent>
       </S.FurtherContainer>
     </S.ExplainContainer>
