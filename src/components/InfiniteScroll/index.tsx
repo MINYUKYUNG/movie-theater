e6111dad5be81feb2828c42d6cb90e70f17ecd85
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2022-09-07 17:11:52 +0900

    :sparkles: Feature: [#14] Laoding 컴포넌트 생성
    Laoding 컴포넌트 생성

--- src/components/InfiniteScroll/index.tsx
+++ src/components/InfiniteScroll/index.tsx
@@ -17,11 +17,15 @@ export default function InfiniteScroll({ callApi }) {
   const scroll = useRef();
   const [loading, setLoading] = useState(true);
 
-  const { data, error, fetchNextPage } = useInfiniteQuery(['posts'], ({ pageParam = 1 }) => callApi(pageParam), {
-    getNextPageParam: (lastPage) => {
-      return lastPage.page + 1;
-    },
-  });
+  const { data, error, fetchNextPage } = useInfiniteQuery(
+    ['posts'],
+    ({ pageParam = 1 }) => callApi(pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage.page + 1;
+      },
+    }
+  );
 
   const io = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -29,16 +33,16 @@ export default function InfiniteScroll({ callApi }) {
         io.unobserve(entry.target);
         fetchNextPage();
       }
-    })
-  })
+    });
+  });
 
   useEffect(() => {
     console.log(callApi);
     scroll.current && io.observe(scroll.current);
   }, [data]);
 
-  if (!data) return <div>loading...</div>
-  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>;
+  if (error) return <div>failed to load</div>;
 
   const result = data.pages.map((page) => {
     return page.results.map((movie: MovieList) => {
@@ -48,12 +52,14 @@ export default function InfiniteScroll({ callApi }) {
           id={movie.id}
           onLoad={() => setLoading(false)}
           imageUrl={
-            loading ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : ''
+            loading
+              ? `https://image.tmdb.org/t/p/original${movie.poster_path}`
+              : ''
           }
           title={movie.title}
           rate={movie.vote_average}
         />
-      )
+      );
     });
   });
 
@@ -63,13 +69,13 @@ export default function InfiniteScroll({ callApi }) {
 
   return (
     <S.ScrollBox>
-      <CardContainer>
-        {result}
-      </CardContainer>
+      <CardContainer>{result}</CardContainer>
       <S.TriangleBox ref={scroll}>
         <S.TriangleImg src={Triangle} alt="Triangle" />
       </S.TriangleBox>
-      <S.ScrollTopBtn onClick={goUp}><FaChevronCircleUp /></S.ScrollTopBtn>
+      <S.ScrollTopBtn onClick={goUp}>
+        <FaChevronCircleUp />
+      </S.ScrollTopBtn>
     </S.ScrollBox>
   );
 }
