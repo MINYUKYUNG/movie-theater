a8cbdfa202e7543d67a199c4c038aabe50ec7421
devfrank9 <devfrank9@gmail.com>
2022-09-07 10:19:47 +0900

    ✨ Feat : 검색 string 받아서 search페이지로 state 전송

--- src/components/commons/layout/Header/style.ts
+++ src/components/commons/layout/Header/style.ts
@@ -2,7 +2,7 @@ import theme from '@styles/theme';
 import styled from 'styled-components';
 
 export const Container = styled.header`
-  width: 100vw;
+  width: 100%;
   padding: 20px 45px;
   display: flex;
   justify-content: space-between;
@@ -10,6 +10,7 @@ export const Container = styled.header`
   background-color: ${theme.colors.SIDE};
 
   > img {
+    cursor: pointer;
     width: auto;
     height: 40px;
   }
