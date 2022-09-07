304de65ac1b85727c033a078ef7d22b7051b63e2
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2022-09-08 03:26:31 +0900

    :sparkles: Feature: index.html favicon 경로 수정 및 toml 파일 추가

--- src/components/Loading/style.ts
+++ src/components/Loading/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 50px;
+  padding-top: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
