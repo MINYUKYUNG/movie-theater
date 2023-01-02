14a401280ce82e5bdfeca6e22f4f1f064cb622ad
MINYUKYUNG <minew0223@gmail.com>
2023-01-02 17:46:28 +0900

    Fix: 영화 리스트 로딩 오류 수정, 컴포넌트 로딩 수정

--- src/components/InfiniteScroll/index.tsx
+++ src/components/InfiniteScroll/index.tsx
@@ -70,21 +70,19 @@ export default function InfiniteScroll({ api, unique }: Infinite) {
   if (error) return <S.LoadingBox>failed to load</S.LoadingBox>;
 
   const CARD_IMG_URL = 'https://image.tmdb.org/t/p/original';
-  const cards =
-    data.pages[0].results.length &&
-    data?.pages.map((page) => {
-      return page.results.map((movie: MovieList) => {
-        return (
-          <Card
-            key={movie.id}
-            id={movie.id}
-            imageUrl={`${CARD_IMG_URL}${movie.poster_path}`}
-            title={movie.title}
-            rate={movie.vote_average}
-          />
-        );
-      });
+  const cards = data?.pages.map((page) => {
+    return page.results.map((movie: MovieList) => {
+      return (
+        <Card
+          key={movie.id}
+          id={movie.id}
+          imageUrl={`${CARD_IMG_URL}${movie.poster_path}`}
+          title={movie.title}
+          rate={movie.vote_average}
+        />
+      );
     });
+  });
 
   const scrollTopBtn = () => {
     if (!up) return null;
@@ -97,11 +95,15 @@ export default function InfiniteScroll({ api, unique }: Infinite) {
 
   return (
     <S.ScrollBox>
-      <CardContainer>{data.pages[0].results.length && cards}</CardContainer>
-      <S.LoadingBox ref={scroll}>
-        <Loading />
-      </S.LoadingBox>
-      {scrollTopBtn()}
+      {data && data.pages[0].results.length && (
+        <>
+          <CardContainer>{cards}</CardContainer>
+          <S.LoadingBox ref={scroll}>
+            <Loading />
+          </S.LoadingBox>
+          {scrollTopBtn()}
+        </>
+      )}
     </S.ScrollBox>
   );
 }
